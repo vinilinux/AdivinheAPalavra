9d3aebcc94e5e4f2b634f87cfd7b5eb885fbf5c2
vinicius lima <vinicius.limalveslinux@gmail.com>
2024-07-20 10:48:38 -0300

    Iniciando o desenvolvimento

---
+++ src/BancoDePalavrasImpl.java
@@ -0,0 +1,11 @@
+public class BancoDePalavrasImpl implements BancoDePalavras{
+
+    @Override
+    public String palavraAleatoria(String nomeDoArquivo) {
+        return "";
+    }
+
+    private void gerarNumeroAleatorio(int maximo) {
+
+    }
+}
