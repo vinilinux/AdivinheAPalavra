f4633f3ded42b2a52911df019c9cc50d7650236c
vinicius lima <vinicius.limalveslinux@gmail.com>
2024-07-23 20:10:43 -0300

    criado a logica do embaralhador de nivel dificil

--- src/main/EmbaralhadorDificil.java
+++ src/main/EmbaralhadorDificil.java
@@ -1,8 +1,26 @@
 package main;
 
+import utils.NumeroAleatorio;
+
 public class EmbaralhadorDificil implements Embaralhador{
+
+    StringBuilder palavraEmbaralhada;
+    NumeroAleatorio numeroAleatorio;
+    public EmbaralhadorDificil() {
+        palavraEmbaralhada = new StringBuilder();
+        numeroAleatorio = new NumeroAleatorio();
+    }
+
     @Override
     public String embaralharPalavra(String palavra) {
-        return "";
+
+        for (int i =0; i < palavra.length(); i++) {
+            palavraEmbaralhada.append(palavra.charAt(numeroAleatorio.gerarNumeroAleatorio(0, palavra.length() - 1)));
+            if (i == 3) {
+                palavraEmbaralhada.append("$");
+            }
+        }
+
+        return palavraEmbaralhada.toString();
     }
 }
